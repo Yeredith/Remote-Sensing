c37de3643aeefaaf7e641dfab7d6706dec6a8152
yeredith <yeredith.luna@gmail.com>
2025-01-09 08:07:16 -0600

    actualizar

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> ef3cf9e75d1c1da1797a6d08e5d3a5d4c332c368
 import torch
 import torch.utils.data as data
 import numpy as np
@@ -9,7 +6,7 @@ import os
 from os.path import join
 import scipy.io as scio
 
-<<<<<<< HEAD
+
 def is_image_file(filename):
     """Función para comprobar si el archivo es una imagen con extensión .mat"""
     return any(filename.endswith(extension) for extension in ['.mat'])
@@ -127,53 +124,3 @@ class ValsetFromFolder2(data.Dataset):
 
     def __len__(self):
         """Devuelve el número total de archivos en el dataset"""
-=======
-
-def is_image_file(filename):
-    return any(filename.endswith(extension) for extension in ['.mat'])
-
-class TestsetFromFolder(data.Dataset):
-    def __init__(self, dataset_dir):
-        super(TestsetFromFolder, self).__init__()
-        self.image_filenames = [join(dataset_dir, x) for x in os.listdir(dataset_dir) if is_image_file(x)]
-
-    def __getitem__(self, index):
-        mat = scio.loadmat(self.image_filenames[index])
-        input = mat['LR'].astype(np.float32).transpose(2, 0, 1)
-        label = mat['HR'].astype(np.float32).transpose(2, 0, 1)
-        image_name = os.path.basename(self.image_filenames[index])  # Obtén el nombre del archivo
-        return torch.from_numpy(input).float(), torch.from_numpy(label).float(), image_name  # Incluye el nombre
-
-    def __len__(self):
-        return len(self.image_filenames)
-
-
-class TrainsetFromFolder(data.Dataset):
-    def __init__(self, dataset_dir):
-        super(TrainsetFromFolder, self).__init__()
-        self.image_filenames = [join(dataset_dir, x) for x in os.listdir(dataset_dir) if is_image_file(x)]
-
-    def __getitem__(self, index):
-        mat = scio.loadmat(self.image_filenames[index], verify_compressed_data_integrity=False)
-        input = mat['lr'].astype(np.float32)
-        label = mat['hr'].astype(np.float32)
-        return torch.from_numpy(input), torch.from_numpy(label)
-
-    def __len__(self):
-        return len(self.image_filenames)
-
-
-class ValsetFromFolder(data.Dataset):
-    def __init__(self, dataset_dir):
-        super(ValsetFromFolder, self).__init__()
-        self.image_filenames = [join(dataset_dir, x) for x in os.listdir(dataset_dir) if is_image_file(x)]
-
-    def __getitem__(self, index):
-        mat = scio.loadmat(self.image_filenames[index])
-        input = mat['LR'].astype(np.float32).transpose(2, 0, 1)
-        label = mat['HR'].astype(np.float32).transpose(2, 0, 1)
-        return torch.from_numpy(input).float(), torch.from_numpy(label).float()
-
-    def __len__(self):
->>>>>>> ef3cf9e75d1c1da1797a6d08e5d3a5d4c332c368
-        return len(self.image_filenames)
